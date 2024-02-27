d3bb9d95d056be29892b70db5320d929acb89e3e
Yabila <cris.yabimaychi@gmail.com>
2024-02-27 10:36:23 -0600

    Tercer Cambio

--- Practica1Git.java
+++ Practica1Git.java
@@ -4,7 +4,7 @@ public class Practica1Git {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Yabila May mi quipo es Monse y Alba");
+		System.out.println("Mi nombre es: Yabila \nMi equipo es: Monse y Alba ");
 	}
 
 }
